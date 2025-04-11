342de8811b059d1eb67240006315a45acd851129
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-04-11 21:20:37 +0100

    Initial Commit

---
+++ src/main/java/com/moushtario/ecomera/EcomeraApplication.java
@@ -0,0 +1,13 @@
+package com.moushtario.ecomera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomeraApplication.class, args);
+	}
+
+}
