f7db248f98ed6cd3f60f0060aa58cd17df76544e
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2025-05-08 00:04:32 +0100

    Persist user generated tokens.

--- src/main/java/com/moushtario/ecomera/auth/AuthenticationService.java
+++ src/main/java/com/moushtario/ecomera/auth/AuthenticationService.java
@@ -1,6 +1,7 @@
 package com.moushtario.ecomera.auth;
 
 import com.moushtario.ecomera.Configuration.JwtService;
+import com.moushtario.ecomera.token.*;
 import com.moushtario.ecomera.user.Role;
 import com.moushtario.ecomera.user.User;
 import com.moushtario.ecomera.user.UserRepository;
@@ -14,12 +15,14 @@ import org.springframework.stereotype.Service;
  * @author Youssef
  * @version 1.0
  * @created 16/04/2025
+ * @lastModified 8/5/2025
  */
 @RequiredArgsConstructor
 @Service
 public class AuthenticationService {
 
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
@@ -38,9 +41,12 @@ public class AuthenticationService {
                 .role(Role.USER)
                 .build();
 
-        userRepository.save(user); // Save the user to the database
+        var savedUser = userRepository.save(user); // Save the user to the database
+
         // Generate a JWT token for the newly registered user
         var jwtToken = jwtService.generateToken(user);
+        // Save the token to the database
+        saveUserToken(savedUser, jwtToken);
 
         return AuthenticationResponse.builder()
                 .token(jwtToken)
@@ -63,8 +69,44 @@ public class AuthenticationService {
                 .orElseThrow(); // Throw any exception not important
 
         var jwtToken = jwtService.generateToken(user); // Generate a JWT token for the authenticated user
+        // Revoke (cancel) all previous tokens for the user
+        revokeAllUserTokens(user);
+        // Save the token to the database
+        saveUserToken(user, jwtToken);
+
+        // Return the generated token in the response
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    /**
+     * Save a user token to the database.
+     * @param user the user to whom the token belongs
+     * @param jwtToken the generated JWT token
+     */
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+    /**
+     * Revoke all tokens for a user.
+     * @param user the user whose tokens will be revoked
+     */
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
     }
 }
