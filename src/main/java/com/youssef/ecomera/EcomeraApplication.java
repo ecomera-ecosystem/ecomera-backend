9e00602dd1322dcb150c9d760cc1cf0ac7e1c453
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2026-01-23 02:27:42 +0100

    feat(app): remove unused command line runner and related imports

--- src/main/java/com/youssef/ecomera/EcomeraApplication.java
+++ src/main/java/com/youssef/ecomera/EcomeraApplication.java
@@ -1,62 +1,11 @@
 package com.youssef.ecomera;
 
-import com.youssef.ecomera.auth.service.AuthenticationService;
-import com.youssef.ecomera.auth.dto.RegisterRequest;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import static com.youssef.ecomera.user.entity.enums.Role.*;
-
-@Slf4j
 @SpringBootApplication
 public class EcomeraApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EcomeraApplication.class, args);
     }
-
-    /**
-     * Command line runner to create an admin and a manager user on startup
-     *
-     * @param authService the authentication service for registering & authenticating users
-     * @return access tokens for an admin and a manager
-     */
-    @Bean
-    public CommandLineRunner commandLineRunner(
-            AuthenticationService authService
-    ) {
-        return args -> {
-            var admin = RegisterRequest.builder()
-                    .firstname("Admin")
-                    .lastname("Admin")
-                    .email("admin@mail.com")
-                    .password("password")
-                    .role(ADMIN)
-                    .build();
-            log.info("Admin access token: {}", authService.register(admin).getAccessToken());
-
-            var manager = RegisterRequest.builder()
-                    .firstname("Manager")
-                    .lastname("Manager")
-                    .email("manager@mail.com")
-                    .password("password")
-                    .role(MANAGER)
-                    .build();
-            log.info("Manager access token: {}", authService.register(manager).getAccessToken());
-
-            var ysf = RegisterRequest.builder()
-                    .firstname("Youssef")
-                    .lastname("Ammari")
-                    .email("youssef.ammari.795@gmail.com")
-                    .password("12345678")
-                    .role(USER)
-                    .build();
-            log.info("YSF access token: {} ", authService.register(ysf).getAccessToken());
-
-        };
-    }
-
 }
