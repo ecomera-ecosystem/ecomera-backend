dcba0b17a9603cf816addf1ae3a3f4086f3c2526
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2026-01-27 17:01:52 +0100

    feat(auth): enhance validation in authentication flow

--- src/main/java/com/youssef/ecomera/auth/dto/AuthenticationRequest.java
+++ src/main/java/com/youssef/ecomera/auth/dto/AuthenticationRequest.java
@@ -1,13 +1,21 @@
 package com.youssef.ecomera.auth.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Builder
 public record AuthenticationRequest(
         @Schema(description = "User email address")
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
         String email,
+
         @Schema(description = "User password")
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters")
         String password
 ) {
 }
