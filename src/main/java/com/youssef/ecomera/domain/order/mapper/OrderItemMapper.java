4dd13ceb2f90fbbf584ca342214ccb75a47550ca
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2026-02-02 21:38:31 +0100

    refactor(mapper): remove updateEntityFromDto

--- src/main/java/com/youssef/ecomera/domain/order/mapper/OrderItemMapper.java
+++ src/main/java/com/youssef/ecomera/domain/order/mapper/OrderItemMapper.java
@@ -4,7 +4,6 @@ import com.youssef.ecomera.common.mapper.BaseMapper;
 import com.youssef.ecomera.common.mapper.BaseMappingConfig;
 import com.youssef.ecomera.domain.order.dto.orderitem.OrderItemCreateDto;
 import com.youssef.ecomera.domain.order.dto.orderitem.OrderItemDto;
-import com.youssef.ecomera.domain.order.dto.orderitem.OrderItemUpdateDto;
 import com.youssef.ecomera.domain.order.entity.OrderItem;
 
 import org.mapstruct.*;
@@ -15,7 +14,6 @@ public interface OrderItemMapper extends BaseMapper<OrderItem, OrderItemDto> {
 
     // === 1. Entity -> DTO (Read) ===
     @Override
-    @Mapping(source = "order.id", target = "orderId")
     @Mapping(source = "product.id", target = "productId")
     OrderItemDto toDto(OrderItem entity);
 
@@ -39,21 +37,7 @@ public interface OrderItemMapper extends BaseMapper<OrderItem, OrderItemDto> {
     @Mapping(target = "updatedBy", ignore = true)
 
     @Mapping(target = "unitPrice", ignore = true) // will be set in service from Product
-    @Mapping(target = "order.id", source = "orderId") //, ignore = true)   // will be set in service
     @Mapping(target = "product.id", source = "productId") //, ignore = true) // will be set in service
+    @Mapping(target = "order", ignore= true)
     OrderItem toEntity(OrderItemCreateDto dto);
-
-    // === 3. DTO -> Existing Entity (Update) ===
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
-    @Mapping(target = "createdBy", ignore = true)
-    @Mapping(target = "updatedBy", ignore = true)
-
-    @Mapping(target = "order", ignore = true)     // not changed on update
-    @Mapping(target = "product", ignore = true)   // set manually in service
-    @Mapping(target = "unitPrice", ignore = true) // re-calculated
-    void updateEntityFromDto(OrderItemUpdateDto dto, @MappingTarget OrderItem entity);
 }
