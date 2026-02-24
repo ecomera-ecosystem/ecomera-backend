fe3f0779a7789bce54d305b2a7892ef737d07bb4
Youssef Ammari <100441472+JoeBroski@users.noreply.github.com>
2026-02-24 14:42:15 +0000

    feat(application): enable scheduling support

--- src/main/java/com/youssef/ecomera/EcomeraApplication.java
+++ src/main/java/com/youssef/ecomera/EcomeraApplication.java
@@ -2,7 +2,9 @@ package com.youssef.ecomera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class EcomeraApplication {
     public static void main(String[] args) {
